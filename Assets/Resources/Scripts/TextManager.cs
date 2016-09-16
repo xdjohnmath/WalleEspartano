dff8b94415348bd26881c788c065c99ba485e034
Adriel <richardd.oci12@gmail.com>
2016-09-16 15:29:24 -0300

    Text file reader and it's scene

---
+++ Assets/Resources/Scripts/TextManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class TextManager : MonoBehaviour {
+
+    public TextAsset text;
+    string allText;
+    List<string> eachline;
+
+    void Start () {
+
+        eachline = new List<string> ();
+
+        allText = text.text;
+
+        eachline.AddRange (allText.Split ("\n"[0]));
+
+        gameObject.GetComponent<Text>().text = eachline[0];
+      
+	}
+	
+
+}
